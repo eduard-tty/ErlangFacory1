6fabb6b7b6206bd5095061ec771aea44b6f5dba1
Eduard Lohmann <eduard@tty.nl>
2011-06-06 14:57:32 +0100

    long way

--- ebank-1.0/src/db_list.erl
+++ ebank-1.0/src/db_list.erl
@@ -6,15 +6,47 @@
 %% @version '1.0'
 %%%---------------------------------------------------------------------
 
--module(ebank_sup).
+-module(db_list).
 -version('1.0').
--behaviour(supervisor).
 -author('eduard@tty.nl <eduard@tty.nl>').
 -copyright('Copyright (c) 2011 Author').
 
 -export([
 	  empty/0
-	, insert/1
+	, insert/2
+	, db_to_list/1
+	, db_size/1
 	, lookup/2
 ]).
 
+-export([test/0]).
+
+-import(lists).
+
+-include("../include/backend.hrl").
+
+
+empty() -> { db , [] }.
+
+insert( Account , { db , Accounts } ) -> 
+	case lists:member(Account, Accounts) of 
+		true  -> {error, excists };
+		false -> { db , [ Account | Accounts ] }
+	end. 
+
+db_to_list( {db, Accounts } ) ->  Accounts.
+
+db_size( {db, Accounts } ) -> length(Accounts).
+
+lookup(AccountNumber, { db, Accounts } ) -> 
+	case lists:keyfind(AccountNumber, 2, Accounts) of
+		false -> {error, instance};
+		Account -> Account
+	end.
+
+test() -> 
+	Db = empty(),
+	Db2 = insert( #account{ no = 1, name = "Eduard"}, Db ),
+	Db3 = insert( #account{ no = 2, name = "Ward"}, Db2 ),
+	lookup(2, Db3).
+	
